50f2ee7310a8af126289f0739a8c6c0599c234e8
Panna Kristof <panna.kristof@prefixbox.com>
2020-12-02 00:46:11 +0100

    "cleanup"

--- AOC2020/dec01/DayOfDec01.cs
+++ AOC2020/dec01/DayOfDec01.cs
@@ -14,31 +14,12 @@ namespace AOC2020.dec01
             Console.WriteLine($"2 numbers: {a} {b}");
             Console.WriteLine($"multiplied: {a * b}");
 
-            FindThreeWithSum(sortedNumbers, 2020, out var x, out var y, out var z);
+            TryFindThreeWithSum(sortedNumbers, 2020, out var x, out var y, out var z);
             Console.WriteLine($"3 numbers: {x} {y} {z}");
             Console.WriteLine($"multiplied: {x * y * z}");
 
             Console.WriteLine("Hello Advent Of Code!");
             Console.ReadKey();
-        }
-
-        private bool FindThreeWithSum(IList<int> sortedNumbers, int targetSum, out int x, out int y, out int z)
-        {
-            // assume: numbers: not null, not empty, SORTED
-            for (int i = 0; i < sortedNumbers.Count; i++)
-            {
-                x = sortedNumbers[i];
-                bool isFound = TryFindTwoWithSum(sortedNumbers.Skip(i).ToList(), targetSum - x, out y, out z);
-                if (isFound)
-                {
-                    return true;
-                }
-            }
-
-            x = 0;
-            y = 0;
-            z = 0;
-            return false;
         }
 
         private bool TryFindTwoWithSum(IList<int> sortedNumbers, int targetSum, out int a, out int b)
@@ -69,5 +50,25 @@ namespace AOC2020.dec01
             b = 0;
             return false;
         }
+
+        private bool TryFindThreeWithSum(IList<int> sortedNumbers, int targetSum, out int x, out int y, out int z)
+        {
+            // assume: numbers: not null, not empty, SORTED
+            for (int i = 0; i < sortedNumbers.Count; i++)
+            {
+                x = sortedNumbers[i];
+                bool isFound = TryFindTwoWithSum(sortedNumbers.Skip(i).ToList(), targetSum - x, out y, out z);
+                if (isFound)
+                {
+                    return true;
+                }
+            }
+
+            x = 0;
+            y = 0;
+            z = 0;
+            return false;
+        }
+
     }
 }
