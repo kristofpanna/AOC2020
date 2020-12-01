76de6527ec976dc04cbe5018033307692becf87e
Panna Kristof <panna.kristof@prefixbox.com>
2020-12-01 23:22:24 +0100

    day 1 part 1

--- AOC2020/dec01/DayOfDec01.cs
+++ AOC2020/dec01/DayOfDec01.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AOC2020.dec01
 {
@@ -7,12 +8,40 @@ namespace AOC2020.dec01
     {
         public void Run(IEnumerable<string> lines)
         {
-            foreach (var line in lines)
-            {
-                Console.WriteLine(line);
-                Console.WriteLine("---");
-            }
+            var numbers = lines.Select(int.Parse);
+            (int a, int b) = FindWithSum(numbers, 2020);
+            Console.WriteLine($"numbers: {a} {b}");
+            Console.WriteLine($"multiplied: {a * b}");
+
             Console.WriteLine("Hello Advent Of Code!");
+            Console.ReadKey();
+        }
+
+        private (int, int) FindWithSum(IEnumerable<int> numbers, int targetSum)
+        {
+            // assume: numbers: not null, not empty
+            var sortedNumbers = numbers.OrderBy(x => x).ToList();
+
+            int small = 0;
+            int big = sortedNumbers.Count - 1;
+            while (small < big)
+            {
+                var sum = sortedNumbers[small] + sortedNumbers[big];
+                if (sum == targetSum)
+                {
+                    return (sortedNumbers[small], sortedNumbers[big]);
+                }
+                if (sum > targetSum)
+                {
+                    big--;
+                }
+                else
+                {
+                    small++;
+                }
+            }
+
+            throw new ArgumentException("No such pair, silly!");
         }
     }
 }
