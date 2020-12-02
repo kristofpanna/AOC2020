dd8aa7c22af5449b43f0b5ef9dea6c595f33f061
Panna Kristof <panna.kristof@prefixbox.com>
2020-12-02 23:34:29 +0100

    day2 part2

--- AOC2020/dec02/DayOfDec02.cs
+++ AOC2020/dec02/DayOfDec02.cs
@@ -8,23 +8,32 @@ namespace AOC2020.dec02
     {
         public void Run(IEnumerable<string> lines)
         {
-            var ret = lines.Count(Valid);
+            var ret = lines.Count(Valid2);
 
             Console.WriteLine($"number of valid pws: {ret}");
             Console.WriteLine("Hello Advent Of Code!");
             Console.ReadKey();
         }
 
-        private bool Valid(string line)
+        private bool Valid1(string line)
         {
             (int min, int max, char letter, string password) = ParseLine(line);
+
             int count = password.Count(c => c == letter);
             return min <= count && count <= max;
         }
 
+        private bool Valid2(string line)
+        {
+            (int i1, int i2, char letter, string password) = ParseLine(line);
+
+            int count = new[] { password[i1 - 1], password[i2 - 1] }.Count(c => c == letter);
+            return count == 1;
+        }
+
         private (int, int, char, string) ParseLine(string line)
         {
-            var things = line.Split(new []{'-', ':', ' '}, StringSplitOptions.RemoveEmptyEntries);
+            var things = line.Split(new[] { '-', ':', ' ' }, StringSplitOptions.RemoveEmptyEntries);
             return (int.Parse(things[0]), int.Parse(things[1]), char.Parse(things[2]), things[3]);
         }
     }
