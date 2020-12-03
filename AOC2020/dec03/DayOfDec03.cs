1b845d8971cbcbf22b30d9a759365f7555035434
Panna Kristof <panna.kristof@prefixbox.com>
2020-12-04 00:18:15 +0100

    day 3 part 2

--- AOC2020/dec03/DayOfDec03.cs
+++ AOC2020/dec03/DayOfDec03.cs
@@ -8,17 +8,35 @@ namespace AOC2020.dec03
     {
         public void Run(IEnumerable<string> lines)
         {
-            int count = CountTreesStraight(lines, 3);
+            // var count = CountTreesEveryLine(lines, 3);
+            // Console.WriteLine($"number of trees on line: {count}");
 
-            Console.WriteLine($"number of trees: {count}");
+            long ret = new[]
+                {
+                    (Right: 1, Down: 1),
+                    (Right: 3, Down: 1),
+                    (Right: 5, Down: 1),
+                    (Right: 7, Down: 1),
+                    (Right: 1, Down: 2)
+                }
+                .Select(p => (long)CountTreesOnSlope(lines, p.Right, p.Down))
+                .Aggregate((a, b) => a * b);
+
+            Console.WriteLine($"multiplied: {ret}");
             Console.WriteLine("Hello Advent Of Code!");
             Console.ReadKey();
         }
 
-        private int CountTreesStraight(IEnumerable<string> lines, int slope)
+        private int CountTreesOnSlope(IEnumerable<string> lines, int rightSlope, int downSlope)
+        {
+            var linesToProcess = lines.Where((line, i) => i % downSlope == 0);
+            return CountTreesEveryLine(linesToProcess, rightSlope);
+        }
+
+        private int CountTreesEveryLine(IEnumerable<string> lines, int slope)
         {
             return lines
-                .Select((line, row) => line[row * slope % line.Length])
+                .Select((line, i) => line[i * slope % line.Length])
                 .Count(IsTree);
         }
 
