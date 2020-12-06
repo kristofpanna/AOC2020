2d089b7b4f2d7f94b7267bc5d8414fd5238101b2
Panna Kristof <panna.kristof@prefixbox.com>
2020-12-06 21:21:42 +0100

    Add simple check tests for day6 (for later)

---
+++ AOC2020Tests/dec06_/DayOfDec06Tests.cs
@@ -0,0 +1,33 @@
+﻿using AOC2020.dec06;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AOC2020Tests.dec06_
+{
+    [TestClass()]
+    public class DayOfDec06Tests
+    {
+        private const string PersonalInput = @"./dec06/test06.txt";
+
+        [TestMethod()]
+        public void TestPart1()
+        {
+            IEnumerable<string> lines = File.ReadLines(PersonalInput);
+
+            var res = DayOfDec06.Part1(lines);
+
+            Assert.AreEqual(6542, res);
+        }
+
+        [TestMethod()]
+        public void TestPart2()
+        {
+            IEnumerable<string> lines = File.ReadLines(PersonalInput);
+
+            var res = DayOfDec06.Part2(lines);
+
+            Assert.AreEqual(3299, res);
+        }
+    }
+}
