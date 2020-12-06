aa1ba59b2ff664404e384a14d0dc3a316db88b00
Panna Kristof <panna.kristof@prefixbox.com>
2020-12-06 20:01:13 +0100

    day 6 part 2

--- AOC2020/dec06/DayOfDec06.cs
+++ AOC2020/dec06/DayOfDec06.cs
@@ -8,25 +8,42 @@ namespace AOC2020.dec06
     {
         public void Run(IEnumerable<string> lines)
         {
-            var res = GetGroupsAnswers(lines, AddStuff)
-                .Select(s => s.Count)
-                .Sum();
+            var res = Part2(lines);
 
             Console.WriteLine(res);
             Console.ReadKey();
         }
 
+        public static int Part1(IEnumerable<string> lines)
+        {
+            return GetGroupsAnswers(lines, () => new HashSet<char>(), AddStuff)
+                .Select(s => s.Count)
+                .Sum(); // 6542
+        }
 
-        private static IEnumerable<ISet<char>> GetGroupsAnswers(IEnumerable<string> lines, Action<ISet<char>, string> updateGroup)
+        public static int Part2(IEnumerable<string> lines)
+        {
+            return GetGroupsAnswers(lines, () => new HashSet<char>(AllLetters()), CommonPart)
+                .Select(s => s.Count)
+                .Sum(); // 3299
+        }
+
+        private static IEnumerable<char> AllLetters()
+        {
+            return Enumerable.Range('a', 'z' - 'a' + 1).Select(c => (char)c);
+        }
+
+
+        private static IEnumerable<ISet<char>> GetGroupsAnswers(IEnumerable<string> lines, Func<ISet<char>> init, Action<ISet<char>, string> updateGroup)
         {
             var groups = new List<ISet<char>>();
-            var group = new HashSet<char>();
+            var group = init();
             foreach (var line in lines)
             {
                 if (line == "")
                 {
                     groups.Add(group);
-                    group = new HashSet<char>();
+                    group = init();
                 }
                 else
                 {
@@ -40,6 +57,11 @@ namespace AOC2020.dec06
         private static void AddStuff(ISet<char> group, string line)
         {
             group.UnionWith(line);
+        }
+
+        private static void CommonPart(ISet<char> group, string line)
+        {
+            group.IntersectWith(line);
         }
     }
 }
