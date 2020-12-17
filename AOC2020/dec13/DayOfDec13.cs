7eb4e50a48aba006f44339fd4b357d46dd253a89
Panna Kristof <panna.kristof@prefixbox.com>
2020-12-17 17:14:32 +0100

    not working euclidean

--- AOC2020/dec13/DayOfDec13.cs
+++ AOC2020/dec13/DayOfDec13.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 
 namespace AOC2020.dec13
 {
@@ -52,52 +53,63 @@ namespace AOC2020.dec13
             return busId - (earliest % busId);
         }
 
-        public static long Part2(IEnumerable<string> lines) // TODO BigInt?
+        public static BigInteger Part2(IEnumerable<string> lines)
         {
             // get linear congruence system
             // all numbers are prime in the inputs -> mods are primes
-            List<(long rem, long mod)> congruences = GetCongruences(lines.Skip(1).First());
+            List<(BigInteger rem, BigInteger mod)> congruences = GetCongruences(lines.Skip(1).First());
 
-            long solution = 0;
-            long mod = 1;
-            foreach (var congruence in congruences)
-            {
-                solution = SolveNext(solution, mod, congruence);
-                mod *= congruence.mod;
-            }
-
+            // https://en.wikipedia.org/wiki/Chinese_remainder_theorem constructive proof
+            var (solution, mod) = congruences.Aggregate<(BigInteger rem, BigInteger mod)>(
+                (acc, next) => (SolveTogether(acc, next), acc.mod * next.mod)
+            );
+            
             return solution;
+
         }
 
-        private static long SolveNext(long solution, long mod, (long rem, long mod) congruence)
-        {
-            return ArithmeticProgression(solution, mod)
-                .First(x => IsSolutionForCongruence(x, congruence));
-        }
-
-        private static IEnumerable<long> ArithmeticProgression(long start, long difference) // infinite
-        {
-            while (true)
-            {
-                yield return start;
-                start += difference;
-            }
-        }
-
-        private static bool IsSolutionForCongruence(long solution, (long rem, long mod) congruence)
-        {
-            return solution % congruence.mod == congruence.rem;
-        }
-
-        private static List<(long rem, long mod)> GetCongruences(string line)
+        private static List<(BigInteger rem, BigInteger mod)> GetCongruences(string line)
         {
             return line.Split(',')
-                .Select((x, i) => (x, i))
-                .Where(t => t.x != "x")
-                .Select(t => (rem: (long.Parse(t.x) - (long)t.i) % long.Parse(t.x), mod: long.Parse(t.x)))
+                .Select((x, i) => (x, i)) // add index
+                .Where(t => t.x != "x") // only numbers
+                .Select(t => (x: BigInteger.Parse(t.x), i: new BigInteger(t.i))) // parse/convert to
+                .Select(t => (rem: (t.x - t.i) % t.x, mod: t.x))
                 .OrderByDescending(t => t.mod)
                 .ToList();
         }
+
+        /// <summary>
+        /// Get solution for the linear system of 2 congruences.
+        /// </summary>
+        private static BigInteger SolveTogether((BigInteger rem, BigInteger mod) cong1, (BigInteger rem, BigInteger mod) cong2)
+        {
+            // want to solve: 
+            //   x = r1 (m1)
+            //   x = r2 (m2)
+            // Bézout:
+            // exists x1, x2:  x1*m1 + x2*m2 = 1  <-- get with Extended Eucl. alg. (primes -> gcd=1)
+            // -> a solution: x1*m1*r2 + r1*x2*m2
+            var (x1, x2) = Euclidean(cong1.mod, cong2.mod);
+            return x1 * cong1.mod * cong2.rem + x2 * cong2.mod * cong1.rem;
+        }
+
+        // x,y: a*x + b*y = gcd(a,b)
+        private static (BigInteger, BigInteger) Euclidean(BigInteger a, BigInteger b)
+        {
+            var (oldR, r) = (a, b);
+            var (oldS, s) = (new BigInteger(1), new BigInteger(0));
+            var (oldT, t) = (new BigInteger(0), new BigInteger(1));
+
+            while (r != 0)
+            {
+                var quotient = oldR / r;
+                (oldR, r) = (r, oldR - quotient * r);
+                (oldS, s) = (s, oldS - quotient * s);
+                (oldT, t) = (t, oldT - quotient * t);
+            }
+
+            return (oldS + oldT, oldT);
+        }
     }
 }
-    
